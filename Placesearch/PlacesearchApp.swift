6e3208cdd53a6d59ce6a1d14b98caf24666c1560
sMoscicki <moscicki.szymonm@gmail.com>
2023-05-10 17:59:43 +0200

    Initial Commit

---
+++ Placesearch/PlacesearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesearchApp.swift
+//  Placesearch
+//
+//  Created by s.Moscicki on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlacesearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
