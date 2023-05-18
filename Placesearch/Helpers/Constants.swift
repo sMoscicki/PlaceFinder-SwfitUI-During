89dbcb2bd4ff2ecc710b9acf8257978617a3737e
sMoscicki <121615393+sMoscicki@users.noreply.github.com>
2023-05-18 18:40:17 +0200

    Update Constants.swift

--- Placesearch/Helpers/Constants.swift
+++ Placesearch/Helpers/Constants.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct Constants {
     
-    static var apiKey = "PZbcG22Xkg2o6ha7pWy5niz72HJkpYQtV-Dh_rhCU_QGs8EcH416zsponFACnRkc5DJZdT-IvTKglJ3pKFV7U_94-jwWYCECeDgtWUwN0b85BG9OWSEzSBO-t2leZHYx"
+    static var apiKey = ""
     static var apiUrl = "https://api.yelp.com/v3/businesses/search"
     static var restaurantKey = "restaurants"
     static var sightsKey = "arts"
